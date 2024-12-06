be877038549a14d7e627c7477dd5f2083ae83e64
bilal <bilalahamed1998@gmail.com>
2024-12-06 11:55:43 +0530

    First commit

--- Tests/login.py
+++ Tests/login.py
@@ -8,7 +8,7 @@ def test_login():
     driver=webdriver.Chrome()
 
     driver.get("https://www.flipkart.com/")
-    time.sleep(5)
+
     text_box = driver.find_element(By.XPATH,value="//input[@placeholder='Search for products, brands and more']")
     text_box.send_keys("Mobile")
     time.sleep(5)
